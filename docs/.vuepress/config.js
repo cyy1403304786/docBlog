9d1a6f1f582981dfcbe65ea776ad107ad7d15530
cyy <2879888343@qq.com>
2020-06-08 11:30:33 +0800

    cyy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    base: "/",   //网站用来部署的基准 URL
+    title: '个人博客', 
+    description: 'Just playing around',
+  
+    head: [
+        ['link', { rel: 'icon', href: '/img/test.ico' }],
+    ],
+    theme: '@vuepress/blog',
+    themeConfig : {
+        
+        sidebar: 'auto',
+        lastUpdated: '上次更新',
+        lastUpdated: true,
+        nav : [
+            {text: 'home',  link: '/',},
+            { text: '接口定义',  link: 'https://www.baidu.com', target:'_self', rel:''},
+            { text: '接口字段定义', link: 'https://www.baidu.com', target:'_self', rel:'' },
+            { text: '附录：错误码', link: 'https://www.baidu.com', target:'_self', rel:'' }
+        ],
+        sidebar: [
+            {
+                title: '开发指南',
+                collapsable: true, //是否展开
+            },
+              ['./guide/install/install','介绍'],
+              ['./guide/started/started','快速上手'],
+        ],
+        sidebarDepth : 2,  //侧边栏自动显示当前激活页面中标题的链接，嵌套在页面本身的链接下。默认深度是1，提取h2标题；0禁用标题链接；最大值为2，同时提取h2和h3。
+
+      },
+      serviceWorker: true
+}
---
+++ docs/README.md
@@ -0,0 +1,17 @@
+---
+home: true
+heroImage: /test.jpg
+heroText: 莹莹的博客啊
+tagline: 做一个精致爱代码的姑娘伢
+actionText: 立即访问
+actionLink: /guide/install/install
+features:
+- title: 简明优先
+  details: 对以 markdown 为中心的项目结构，做最简化的配置，帮助你专注于创作。
+- title: Vue 驱动
+  details: 享用 Vue + webpack 开发环境，在 markdown 中使用 Vue 组件，并通过 Vue 开发自定义主题。
+- title: 性能高效
+  details: VuePress 将每个页面生成为预渲染的静态 HTML，每个页面加载之后，然后作为单页面应用程序(SPA)运行。
+  footer: MIT Licensed | Copyright © 2018-present Evan You
+---
+
