e24926a6f41c96f7dd0748ecf540995ffe221508
cyy <2879888343@qq.com>
2021-03-14 22:23:56 +0800

    cyy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,6 +23,7 @@ module.exports = {
                     [ 
                       {title:'css 重要知识点', path: './guide/started/css'},
                       {title:'js 重要知识点', path: './guide/started/js'},
+                      {title:'vue 重要知识点', path: './guide/started/vue'},
                       {title:'webhook自动部署', path: './guide/started/webhook'},
                       {title:'自定义代码片段', path: './guide/started/snipt'},
                       {title:'echarts图表', path: './guide/started/echarts'},
